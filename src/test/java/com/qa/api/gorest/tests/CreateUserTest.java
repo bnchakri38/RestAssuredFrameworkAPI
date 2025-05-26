ade7fa1170cb91520b4e85a2f4ba03e794ce29cf
bnchakri38 <bnchakri38@gmail.com>
2025-05-27 00:07:34 +0530

    GoRestAPI Users - Post Requests

--- src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
+++ src/test/java/com/qa/api/gorest/tests/CreateUserTest.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import com.qa.api.base.BaseTest;
@@ -18,6 +19,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 
 public class CreateUserTest extends BaseTest {
+	
+	String tokenId;
+	
+	@BeforeClass
+	public void setUpToken() {
+		tokenId = "19f98d9b819ddd2907f1a3e1033c44cae4b50f11f1967b9cd3b6435a7d906e13";
+		ConfigManager.set("bearertoken", tokenId);
+	}
 	
 	@Test
 	public void createAUserTestWithPOJO() {
